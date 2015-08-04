5b7f7586c28287ab399365dbcaf0364e37637c6c
Arne Zeising <arne.zeising@gmail.com>
2015-08-04 16:24:25 +0200

    Removed line breaks
    The backslashes to escape line breaks in the curl call for some reason did not work for me on codeship. Maybe this helps somebody else.

--- notifications/new_relic.sh
+++ notifications/new_relic.sh
@@ -28,10 +28,4 @@ NEW_RELIC_DESCRIPTION=$(echo ${NEW_RELIC_DESCRIPTION//;/%3B}|sed ':a;N;$!ba;s/\n
 NEW_RELIC_REVISION=$(echo ${NEW_RELIC_REVISION//;/%3B}|sed ':a;N;$!ba;s/\n/ /g'|tr -s ' ')
 NEW_RELIC_CHANGELOG=$(echo ${NEW_RELIC_CHANGELOG//;/%3B}|sed ':a;N;$!ba;s/\n/ /g'|tr -s ' ')
 
-curl https://api.newrelic.com/deployments.xml \
-	-H "x-api-key:${NEW_RELIC_API_KEY}" \
-	-d "deployment[app_name]=${NEW_RELIC_APP}" \
-	-d "deployment[description]=${NEW_RELIC_DESCRIPTION}" \
-	-d "deployment[revision]=${NEW_RELIC_REVISION}" \
-	-d "deployment[changelog]=${NEW_RELIC_CHANGELOG}" \
-	-d "deployment[user]=${CI_COMMITTER_USERNAME}"
+curl -H "x-api-key:${NEW_RELIC_API_KEY}" -d "deployment[app_name]=${NEW_RELIC_APP}" -d "deployment[description]=${NEW_RELIC_DESCRIPTION}" -d "deployment[revision]=${NEW_RELIC_REVISION}" -d "deployment[changelog]=${NEW_RELIC_CHANGELOG}" -d "deployment[user]=${CI_COMMITTER_USERNAME}" https://api.newrelic.com/deployments.xml
